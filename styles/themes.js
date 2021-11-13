34a42be9e5ccbd96fc13eca6541de808d7fe13c3
ddjb77 <67380362+ddjb77@users.noreply.github.com>
2021-11-13 00:30:29 -0300

    Subindo AppFrases

---
+++ styles/themes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {StyleSheet} from 'react-native';
+
+const darkMode = StyleSheet.create({
+    card: {
+        flex: 1,
+        justifyContent: "center",
+        width: "100%",
+        backgroundColor:"#000",
+        color: "#fff",
+      },
+
+});
+
+export default darkMode;
